9e5c0b0577ad367f20ae683730851b36f11dbcc1
Repaka Bhargav <repaka.bhargav@gmail.com>
2024-01-28 23:44:41 +0530

    cors

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGODB_URI, { dbName:"task-management-devtown"})
 
 //middleware
 app.use(express.json())
-app.use(cors({ origin: "*" }))
+app.use(cors())
 app.use(morgan("tiny"))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
